94ef89215f4da6d0a2792093b7d8a94987dcaa85
Abel Trotter and Kelli Brooke <abel@>
2018-06-11 10:31:06 -0700

    Add place display logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,29 @@ var bend = new Place("Bend", "USA", "English");
 var barcelona = new Place("Barcelona", "Spain", "Spanish");
 var rivendell = new Place("Rivendell", "Middle Earth", "Elvish");
 
+function displayPlace(element) {
+  var placeName = element.id;
+  var bendText = "Country: " + bend.country + "<br>Main Language: " + bend.mainLanguage;
+  var barcelonaText = "Country: " + barcelona.country + "<br>Main Language: " + barcelona.mainLanguage;
+  var rivendellText = "Country: " + rivendell.country + "<br>Main Language: " + rivendell.mainLanguage;
+
+  if (placeName === "bend") {
+    showPlaceDetails(element, bendText);
+  } else if (placeName === "barcelona") {
+    showPlaceDetails(element, barcelonaText);
+  } else if (placeName === "rivendell") {
+    showPlaceDetails(element, rivendellText);
+  } else {
+    alert("Error! No, not telling you why!");
+  }
+}
+
+function showPlaceDetails(element, text) {
+  $(element).siblings("div.details").html(text);
+}
+
 $(document).ready(function() {
-  console.log("loaded");
+  $(".btn-place").click(function() {
+    displayPlace(this);
+  });
 });
